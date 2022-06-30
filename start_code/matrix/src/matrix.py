3ff87204b77c14dc42934393fc24b8fa55e7fc3e
Andrew <andrewfairley95@gmail.com>
2022-06-30 15:22:17 +0100

    matrix started

--- start_code/matrix/src/matrix.py
+++ start_code/matrix/src/matrix.py
@@ -1,9 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
 
     def row(self, index):
-        pass
+        rows = self.matrix_string.split("\n")
+        for number in rows:
+            int(number)
+        return rows
+            
 
     def column(self, index):
         pass
